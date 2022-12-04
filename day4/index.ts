6ced2bfc535ac90b7aa5241be80895aaf9bb9658
Dima Kasianov <korvin1986@gmail.com>
2022-12-04 06:20:01 +0100

    Day 4

---
+++ day4/index.ts
@@ -0,0 +1,61 @@
+import { readFileSync } from 'fs';
+
+const readInput = () => {
+    const data = readFileSync('day4/input.txt', 'utf8');
+    return data.split('\n')
+}
+
+
+const isPairsFullyOverlapping = (a: string, b: string) => {
+    const pair1 = a.split('-').map(n => parseInt(n));
+    const pair2 = b.split('-').map(n => parseInt(n));
+
+    if (pair1[0] >= pair2[0] && pair1[1] <= pair2[1]) {
+        return true
+    }
+
+    if (pair2[0] >= pair1[0] && pair2[1] <= pair1[1]) {
+        return true
+    }
+
+    return false
+}
+
+const isPairsPartialOverlapping = (a: string, b: string) => {
+    const pair1 = a.split('-').map(n => parseInt(n));
+    const pair2 = b.split('-').map(n => parseInt(n));
+
+    if (pair1[0] >= pair2[0] && pair1[0] <= pair2[1]) {
+        return true
+    }
+
+    if (pair1[1] >= pair2[0] && pair1[1] <= pair2[1]) {
+        return true
+    }
+
+    if (pair2[0] >= pair1[0] && pair2[0] <= pair1[1]) {
+        return true
+    }
+
+    if (pair2[1] >= pair1[0] && pair2[1] <= pair1[1]) {
+        return true
+    }
+
+    return false
+}
+
+const Task1 = () => {
+    return readInput().map(line => {
+        const pairs = line.split(',');
+        return isPairsFullyOverlapping(pairs[0], pairs[1]) ? 1 : 0
+    }).reduce((acc, curr) => acc + curr, 0)
+}
+
+const Task2 = () => {
+    return readInput().map(line => {
+        const pairs = line.split(',');
+        return isPairsPartialOverlapping(pairs[0], pairs[1]) ? 1 : 0
+    }).reduce((acc, curr) => acc + curr, 0)
+}
+
+export { Task1, Task2 }
